6575822b0ff2c51cc61f996637508163d7c46e88
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2024-10-01 01:04:13 +0530

    added read, write, append, delete & stats

--- file.js
+++ file.js
@@ -1,7 +1,30 @@
+const { error } = require("console");
 const fs = require("fs");
 
 // Sync...
 // fs.writeFileSync("./test.txt", "Hello world");
 
 // Async
-fs.writeFile("./test.txt", "Hello world Async", (error) => {});
+// fs.writeFile("./test.txt", "Hello world Async", (error) => {});
+
+// const result = fs.readFileSync("./contact.txt", "utf-8");
+// console.log(result);
+
+// fs.readFile("./contact.txt", "utf-8", (error, result) => {
+//   if (error) {
+//     console.log("Error", error);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// fs.appendFileSync("./test.txt", `${Date.now()} Hey there!\n`);
+
+// copy the file
+// fs.cpSync("./test.txt", "copy.txt");
+
+// delete the file
+// fs.unlinkSync("./copy.txt");
+
+// Show the status of the file
+console.log(fs.statSync("./test.txt"));
