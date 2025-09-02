fb5a6c25f21fe56e1718d4e1b0c7cab4f7322110
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-09-02 15:48:04 +0530

    tricky interview question and event loop practice

---
+++ event-loop-3.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+setImmediate(() => console.log('setImmediate'));
+
+setTimeout(() => console.log('Timer Expired'), 0);
+
+Promise.resolve('Promise').then(console.log);
+
+fs.readFile('./test.txt', 'utf8', () => {
+  setTimeout(() => console.log('2nd timer'), 0);
+
+  process.nextTick(() => console.log('2nd nextTick'));
+
+  setImmediate(() => console.log('2nd setImmediate'));
+
+  console.log('File reading CB');
+});
+
+process.nextTick(() => console.log('nextTick'));
+
+console.log('Last line of the file.');
+
+//! output
+//* Last line of the file.
+//* nextTick
+//* Promise
+//* Timer Expired
+//* setImmediate
+//* File reading CB
+//* 2nd nextTick
+//* 2nd setImmediate
+//* 2nd timer
