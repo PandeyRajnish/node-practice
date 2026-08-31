e6684eeed26ac783efa335934bcb7767fe20b0ae
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2026-08-31 14:22:32 +0530

    implement thread pool example using crypto.pbkdf2 for concurrent processing

---
+++ thread-pool.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+process.env.UV_THREADPOOL_SIZE = 2;
+
+crypto.pbkdf2('password', 'salt', 50000, 1024, 'sha512', (err, key) => {
+  console.log('1 - crypto.pbkdf2 done');
+});
+
+crypto.pbkdf2('password', 'salt', 50000, 1024, 'sha512', (err, key) => {
+  console.log('2 - crypto.pbkdf2 done');
+});
+
+crypto.pbkdf2('password', 'salt', 50000, 1024, 'sha512', (err, key) => {
+  console.log('3 - crypto.pbkdf2 done');
+});
+
+crypto.pbkdf2('password', 'salt', 50000, 1024, 'sha512', (err, key) => {
+  console.log('4 - crypto.pbkdf2 done');
+});
+
+crypto.pbkdf2('password', 'salt', 50000, 1024, 'sha512', (err, key) => {
+  console.log('5 - crypto.pbkdf2 done');
+});
